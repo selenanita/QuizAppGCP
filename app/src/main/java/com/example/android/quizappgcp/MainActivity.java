a76b169b125d55bca6499917f0332ee249ef2508
Selena <selenanita@gmail.com>
2018-04-14 00:23:04 +0300

    Quiz App

---
+++ app/src/main/java/com/example/android/quizappgcp/MainActivity.java
@@ -0,0 +1,220 @@
+package com.example.android.quizappgcp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+// Photo sky2 by PhotoMIX Ltd. from Pexels
+// Photo gcp_image from https://www.raps.org/regulatory-focus%E2%84%A2/news-articles/2015/9/amended-ich-gcp-guideline-addresses-evolution-of-trials-landscape
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * This is quiz app GCP. Checks correct answers and display the score .
+     */
+
+    //Global values
+
+    int score1;
+    int score2;
+    int score3;
+    int score4;
+    int score5;
+    int score6;
+    int score7;
+    int scoreTotal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitAnswer(View view) {
+
+        RadioButton radioButtonQ1A1 = findViewById(R.id.q1a1);
+        boolean answerClickedQ1 = radioButtonQ1A1.isChecked();
+        calculateScoreQ1(answerClickedQ1);
+
+        RadioButton radioButtonQ2A3 = findViewById(R.id.q2a3);
+        boolean answerClickedQ2 = radioButtonQ2A3.isChecked();
+        calculateScoreQ2(answerClickedQ2);
+
+        CheckBox checkBoxQ3A1 = findViewById(R.id.q3a1);
+        boolean answerClickedQ3A1 = checkBoxQ3A1.isChecked();
+
+        CheckBox checkBoxQ3A2 = findViewById(R.id.q3a2);
+        boolean answerClickedQ3A2 = checkBoxQ3A2.isChecked();
+
+        CheckBox checkBoxQ3A3 = findViewById(R.id.q3a3);
+        boolean answerClickedQ3A3 = checkBoxQ3A3.isChecked();
+
+        CheckBox checkBoxQ3A4 = findViewById(R.id.q3a4);
+        boolean answerClickedQ3A4 = checkBoxQ3A4.isChecked();
+        calculateScoreQ3(answerClickedQ3A1, answerClickedQ3A2, answerClickedQ3A3, answerClickedQ3A4);
+
+        RadioButton radioButtonQ4A2 = findViewById(R.id.q4a2);
+        boolean answerClickedQ4 = radioButtonQ4A2.isChecked();
+        calculateScoreQ4(answerClickedQ4);
+
+        RadioButton radioButtonQ5A3 = findViewById(R.id.q5a3);
+        boolean answerClickedQ5 = radioButtonQ5A3.isChecked();
+        calculateScoreQ5(answerClickedQ5);
+
+        CheckBox checkBoxQ6A1 = findViewById(R.id.q6a1);
+        boolean answerClickedQ6A1 = checkBoxQ6A1.isChecked();
+
+        CheckBox checkBoxQ6A2 = findViewById(R.id.q6a2);
+        boolean answerClickedQ6A2 = checkBoxQ6A2.isChecked();
+
+        CheckBox checkBoxQ6A3 = findViewById(R.id.q6a3);
+        boolean answerClickedQ6A3 = checkBoxQ6A3.isChecked();
+
+        CheckBox checkBoxQ6A4 = findViewById(R.id.q6a4);
+        boolean answerClickedQ6A4 = checkBoxQ6A4.isChecked();
+        calculateScoreQ6(answerClickedQ6A1, answerClickedQ6A2, answerClickedQ6A3, answerClickedQ6A4);
+
+        EditText editTextQ7 = findViewById(R.id.answer_input);
+        String answerQ7 = editTextQ7.getText().toString();
+        calculateScoreQ7(answerQ7);
+
+        scoreTotal = calculateTotalScore(score1, score2, score3, score4, score5, score6, score7);
+
+        if (scoreTotal == 7) {
+            Toast.makeText(this, "Good job. You did it! Your score is " + scoreTotal + " out of 7!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Keep trying. Your score is " + scoreTotal + " out of 7!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * this method check if the user checked the correct button for question 1
+     */
+    public void calculateScoreQ1(boolean answerClickedQ1) {
+
+        score1 = 0;
+        if (answerClickedQ1) {
+            score1 = 1;
+        }
+    }
+
+    /**
+     * this method check if the user checked the correct button for question 2
+     */
+    public void calculateScoreQ2(boolean answerClickedQ2) {
+
+        score2 = 0;
+        if (answerClickedQ2) {
+            score2 = 1;
+        }
+    }
+
+    /**
+     * this method check if the user checked the correct check box for question 3
+     */
+
+    public void calculateScoreQ3(boolean answerClickedQ3A1, boolean answerClickedQ3A2, boolean answerClickedQ3A3, boolean answerClickedQ3A4) {
+        score3 = 0;
+        if (answerClickedQ3A1 && !answerClickedQ3A2 && !answerClickedQ3A3 && answerClickedQ3A4) {
+            score3 = 1;
+        }
+    }
+
+    /**
+     * this method check if the user checked the correct button for question 4
+     */
+    public void calculateScoreQ4(boolean answerClickedQ4) {
+
+        score4 = 0;
+        if (answerClickedQ4) {
+            score4 = 1;
+        }
+    }
+
+    /**
+     * this method check if the user checked the correct button for question 5
+     */
+    public void calculateScoreQ5(boolean answerClickedQ5) {
+
+        score5 = 0;
+        if (answerClickedQ5) {
+            score5 = 1;
+        }
+    }
+
+    /**
+     * this method check if the user checked the correct check box for question 6
+     */
+
+    public void calculateScoreQ6(boolean answerClickedQ6A1, boolean answerClickedQ6A2, boolean answerClickedQ6A3, boolean answerClickedQ6A4) {
+        score6 = 0;
+        if (answerClickedQ6A1 && answerClickedQ6A2 && answerClickedQ6A3 && answerClickedQ6A4) {
+            score6 = 1;
+        }
+    }
+
+    /**
+     * this method check if the user answered correct for question 7
+     */
+
+    public void calculateScoreQ7(String answerQ7) {
+        score7 = 0;
+        if (answerQ7.equals("Case Report Form") || answerQ7.equals("CASE REPORT FORM") || answerQ7.equals("case report form")) {
+            score7 = 1;
+        }
+    }
+
+    /**
+     * this method calculates the total scores
+     */
+
+    public int calculateTotalScore(int score1, int score2, int score3, int score4, int score5, int score6, int score7) {
+        return score1 + score2 + score3 + score4 + score5 + score6 + score7;
+    }
+
+    public void reset(View view){
+        RadioGroup radioGroup1 = findViewById(R.id.RGroup1);
+        radioGroup1.clearCheck();
+
+        RadioGroup radioGroup2 = findViewById(R.id.RGroup2);
+        radioGroup2.clearCheck();
+
+        RadioGroup radioGroup3 = findViewById(R.id.RGroup3);
+        radioGroup3.clearCheck();
+
+        RadioGroup radioGroup4 = findViewById(R.id.RGroup4);
+        radioGroup4.clearCheck();
+
+        CheckBox checkBoxQ3A1 = findViewById(R.id.q3a1);
+        checkBoxQ3A1.setChecked(false);
+
+        CheckBox checkBoxQ3A2 = findViewById(R.id.q3a2);
+        checkBoxQ3A2.setChecked(false);
+
+        CheckBox checkBoxQ3A3 = findViewById(R.id.q3a3);
+        checkBoxQ3A3.setChecked(false);
+
+        CheckBox checkBoxQ3A4 = findViewById(R.id.q3a4);
+        checkBoxQ3A4.setChecked(false);
+
+        CheckBox checkBoxQ6A1 = findViewById(R.id.q6a1);
+        checkBoxQ6A1.setChecked(false);
+
+        CheckBox checkBoxQ6A2 = findViewById(R.id.q6a2);
+        checkBoxQ6A2.setChecked(false);
+
+        CheckBox checkBoxQ6A3 = findViewById(R.id.q6a3);
+        checkBoxQ6A3.setChecked(false);
+
+        CheckBox checkBoxQ6A4 = findViewById(R.id.q6a4);
+        checkBoxQ6A4.setChecked(false);
+
+        EditText editTextQ7 = findViewById(R.id.answer_input);
+        editTextQ7.setText("");
+    }
+}
